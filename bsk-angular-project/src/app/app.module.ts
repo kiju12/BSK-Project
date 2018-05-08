69d0ad0f47df04f151037f9e3205f0d0061bb170
adamkrupa96@gmail.com <adamkrupa96@gmail.com>
2018-05-09 01:18:48 +0200

    add components to app module. add global styles to styles.scss

--- bsk-angular-project/src/app/app.module.ts
+++ bsk-angular-project/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { MainNavbarComponent } from './modules/main-navbar/main-navbar.component';
+import { MainFooterComponent } from './modules/main-footer/main-footer.component';
+import { MainPageContentComponent } from './modules/main-page-content/main-page-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavbarComponent,
+    MainFooterComponent,
+    MainPageContentComponent
   ],
   imports: [
     BrowserModule,
